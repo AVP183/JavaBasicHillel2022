669251c5987deb1114f7bbf95e9bc50faa39b635
avp <nichushkin@ukr.net>
2022-04-03 22:34:53 +0300

    доработал конструктор

--- src/com/company/Box.java
+++ src/com/company/Box.java
@@ -1,22 +1,84 @@
 package com.company;
 
 public class Box {
-    int length;
-    int width;
-    int height;
+    private int length;
+    private int width;
+    private int height;
+    private String color;
+    private String material;
 
-    Box(int length, int width, int height) {
+    public Box(int length, int width, int height) {
         this.length = length;
         this.width = width;
         this.height = height;
     }
 
-    int capacity(int a, int b, int c) {
+    public Box(int length, int width, int height, String color, String material) {
+        this(length, width, height);
+        this.color = color;
+        this.material = material;
+    }
+
+    public Box() {
+        
+    }
+
+    public void setLength(int length) {
+        if (length > 0) {
+            this.length = length;
+        }
+        System.out.println("Коробка не может быть создана с размерами меньше 1 см");
+            return;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public int capacity(int a, int b, int c) {
         return a * b * c;
     }
 
-    void printBox(int a, int b, int c) {
-        System.out.printf("Мы создали коробку с длиной %2d см, с шириной %2d см и высотой %2d см. Объем коробки " +
-                "составит %7d см^3\n", length, width, height, capacity(length, width, height));
+    public void printBox(int a, int b, int c) {
+        System.out.printf("Мы создали коробку с длиной %3d см, с шириной %3d см и высотой %3d см. Объем коробки " +
+                "составит %6d см^3\n", length, width, height, capacity(length, width, height));
+    }
+
+    public String printBoxColorMaterial() {
+        return String.format("Мы создали коробку с длиной %3d см, с шириной %3d см и высотой %3d см. Объем коробки " +
+                        "составит %6d см^3, цвет коробки: %7s, материал: %8s",
+                length, width, height, capacity(length, width, height), color, material);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,22 +6,34 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Box box1 = new Box(randomValue(), randomValue(), randomValue());
-        Box box2 = new Box(randomValue(), randomValue(), randomValue());
-        Box box3 = new Box(randomValue(), randomValue(), randomValue());
-        Box box4 = new Box(randomValue(), randomValue(), randomValue());
-        Box box5 = new Box(randomValue(), randomValue(), randomValue());
+//        Box box1 = new Box(randomValue(), randomValue(), randomValue(), "черная", "дерево");
+//        Box box2 = new Box(randomValue(), randomValue(), randomValue(), "белая", "пластик");
+//        Box box3 = new Box(randomValue(), randomValue(), randomValue(), "синяя", "картон");
+//        Box box4 = new Box(randomValue(), randomValue(), randomValue(), "зеленая", "текстиль");
+//        Box box5 = new Box(randomValue(), randomValue(), randomValue(), "красная", "металл");
+        Box box1 = new Box();
+        box1.setLength(-2);
+        box1.setWidth(randomValue());
+        box1.setHeight(randomValue());
+        box1.setColor("black");
+        box1.setMaterial("plastic");
 
-        box1.printBox(box1.length, box1.width, box1.height);
-        box2.printBox(box2.length, box2.width, box2.height);
-        box3.printBox(box3.length, box3.width, box3.height);
-        box4.printBox(box4.length, box4.width, box4.height);
-        box5.printBox(box5.length, box5.width, box5.height);
 
+//        box1.printBox(box1.getLength(), box1.getWidth(), box1.getHeight());
+//        box2.printBox(box2.getLength(), box2.getWidth(), box2.getHeight());
+//        box3.printBox(box3.getLength(), box3.getWidth(), box3.getHeight());
+//        box4.printBox(box4.getLength(), box4.getWidth(), box4.getHeight());
+//        box5.printBox(box5.getLength(), box5.getWidth(), box5.getHeight());
+
+        System.out.println(box1.printBoxColorMaterial());
+//        System.out.println(box2.printBoxColorMaterial());
+//        System.out.println(box3.printBoxColorMaterial());
+//        System.out.println(box4.printBoxColorMaterial());
+//        System.out.println(box5.printBoxColorMaterial());
     }
 
     public static int randomValue() {
         Random random = new Random();
-        return random.nextInt(1, 100);
+        return random.nextInt(1, 101);
     }
 }

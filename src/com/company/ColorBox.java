d873d7fa0e0392dfefc0dc0ae66670797e422af2
avp183 <nichushkin@ukr.net>
2022-04-08 15:35:47 +0300

    Home Work 13 добавил класс ColorBox и создал enum ColorVariant

--- src/com/company/ColorBox.java
+++ src/com/company/ColorBox.java
@@ -1,28 +1,27 @@
 package com.company;
 
-import java.awt.Color;
-
 public class ColorBox extends Box {
-    private String color;
+    private ColorVariant color;
 
-    public ColorBox(int length, int width, int height, String material, String color) {
+    public ColorBox(int length, int width, int height, String material, ColorVariant color) {
         super(length, width, height, material);
         this.color = color;
     }
 
-    public void setColor(String color) {
+    public void setColor(ColorVariant color) {
         this.color = color;
     }
 
-    public String getColor() {
+    public ColorVariant getColor() {
         return color;
     }
 
-    public String printColorBox() {
+    public String printBoxColorMaterial() {
         return String.format("Мы создали коробку с длиной %3d см, с шириной %3d см и высотой %3d см. Объем коробки " +
-                        "составит %6d см^3, материал: %8s, цвет: %s",
+                        "составит %6d см^3, материал: %8s, color: %s",
                 super.getLength(), super.getWidth(), super.getHeight(), capacity(super.getLength(), super.getWidth(),
                         super.getHeight()), super.getMaterial(), color);
     }
+
 
 }

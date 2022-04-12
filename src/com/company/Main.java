6acfd04c16bf84ce8b38ebb344f00dac8b1fcf23
avp183 <nichushkin@ukr.net>
2022-04-12 14:26:26 +0300

    Home Work 14 work with Warehouse

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,63 +6,79 @@ public class Main {
 
     public static void main(String[] args) {
 
-//
-//        try {
-//            ColorBox colorBox1 = new ColorBox(randomValue(), randomValue(), randomValue(),
-//                    Material.getMatirial(Material.randomMatirial()), ColorVariant.getColor(ColorVariant.randomColor()));
-//            System.out.println(colorBox1.toString());
-//        } catch (IllegalArgumentException e) {
-//            System.out.println(e.getMessage());
-//        }
-//
-//        try {
-//            ColorBox colorBox2 = new ColorBox(randomValue(), randomValue(), randomValue(),
-//                    Material.getMatirial(Material.randomMatirial()), ColorVariant.getColor(ColorVariant.randomColor()));
-//            System.out.println(colorBox2.toString());
-//        } catch (IllegalArgumentException e) {
-//            System.out.println(e.getMessage());
-//        }
-//
-//        try {
-//            ColorBox colorBox3 = new ColorBox(randomValue(), randomValue(), randomValue(),
-//                    Material.getMatirial(Material.randomMatirial()), ColorVariant.getColor(ColorVariant.randomColor()));
-//            System.out.println(colorBox3.toString());
-//        } catch (IllegalArgumentException e) {
-//            System.out.println(e.getMessage());
-//        }
-//
-//        try {
-//            ColorBox colorBox4 = new ColorBox(randomValue(), randomValue(), randomValue(),
-//                    Material.getMatirial(Material.randomMatirial()), ColorVariant.getColor(ColorVariant.randomColor()));
-//            System.out.println(colorBox4.toString());
-//        } catch (IllegalArgumentException e) {
-//            System.out.println(e.getMessage());
-//        }
-//
-//        try {
-//            ColorBox colorBox5 = new ColorBox(randomValue(), randomValue(), randomValue(),
-//                    Material.getMatirial(Material.randomMatirial()), ColorVariant.getColor(ColorVariant.randomColor()));
-//            System.out.println(colorBox5.toString());
-//        } catch (IllegalArgumentException e) {
-//            System.out.println(e.getMessage());
-//        }
+
+        try {
+            ColorBox colorBox1 = new ColorBox(randomValue(), randomValue(), randomValue(),
+                    Material.getMatirial(Material.randomMatirial()), ColorVariant.getColor(ColorVariant.randomColor()));
+            System.out.println(colorBox1.toString());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            ColorBox colorBox2 = new ColorBox(randomValue(), randomValue(), randomValue(),
+                    Material.getMatirial(Material.randomMatirial()), ColorVariant.getColor(ColorVariant.randomColor()));
+            System.out.println(colorBox2.toString());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            ColorBox colorBox3 = new ColorBox(randomValue(), randomValue(), randomValue(),
+                    Material.getMatirial(Material.randomMatirial()), ColorVariant.getColor(ColorVariant.randomColor()));
+            System.out.println(colorBox3.toString());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            ColorBox colorBox4 = new ColorBox(randomValue(), randomValue(), randomValue(),
+                    Material.getMatirial(Material.randomMatirial()), ColorVariant.getColor(ColorVariant.randomColor()));
+            System.out.println(colorBox4.toString());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            ColorBox colorBox5 = new ColorBox(randomValue(), randomValue(), randomValue(),
+                    Material.getMatirial(Material.randomMatirial()), ColorVariant.getColor(ColorVariant.randomColor()));
+            System.out.println(colorBox5.toString());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
 
 //        box1.printCountBoxes();
 //        box1.checkOrder();
 
+        try {
+            Warehouse b1 = new Warehouse(2,1,1,1);
+            System.out.println(Warehouse.printArrayBoxes());
+            System.out.println(Warehouse.valueWarehouseLeft());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
 
-        Warehouse warehouse = new Warehouse(3);
+
+        Warehouse b2 = new Warehouse(3,2,3,4);
         System.out.println(Warehouse.printArrayBoxes());
         System.out.println(Warehouse.valueWarehouseLeft());
-        Warehouse warehouse2 = new Warehouse(6);
+        Warehouse.addBoxToWarehouse(3);
         System.out.println(Warehouse.printArrayBoxes());
         System.out.println(Warehouse.valueWarehouseLeft());
+
+        Warehouse.addBoxToWarehouse(22);
+        Warehouse.addBoxToWarehouse(88);
+        System.out.println(Warehouse.printArrayBoxes());
+        System.out.println(Warehouse.valueWarehouseLeft());
+
+//        System.out.println(Ware
 
     }
 
     public static int randomValue() {
         Random random = new Random();
-        return random.nextInt(-20, 101);
+        return random.nextInt(1, 101);
     }
 
 
--- src/com/company/Warehouse.java
+++ src/com/company/Warehouse.java
@@ -1,25 +1,50 @@
 package com.company;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Warehouse {
-    private static int capacityWarehouse = 100;
+    private static final int capacityWarehouse = 30;
     private static int counterWarehouse;
-    Box[] arrayAllBox;
+    private int capacity;
+    private static Box[] arrayAllBox;
+    private int length;
+    private int width;
+    private int height;
 
-    public Warehouse(int capacity) {
-        Box[] arrayAllBox = new Box[capacity];
-        for (int i = 0; i < arrayAllBox.length; i++) {
-            counterWarehouse++;
-            arrayAllBox[i] = new Box(random(), random(), random());
-        }
-        for (int i = 0; i < arrayAllBox.length; i++) {
-            System.out.println(arrayAllBox[i]);
+    public Warehouse(int length, int width, int height, int capacity) {
+            if (length <= 0 || width <= 0 || height <= 0) {
+                throw new IllegalArgumentException("Ошибка: Проверьте вводимые параметры Д х Ш х В");
+            }
+        arrayAllBox = new Box[capacity];
+        if (counterWarehouse < capacityWarehouse) {
+            for (int i = 0; i < capacity; i++) {
+                counterWarehouse++;
+                arrayAllBox[i] = new Box(length, width, height);
+                System.out.println(arrayAllBox[i]);
+                if (counterWarehouse == capacityWarehouse) {
+                    break;
+                }
+            }
+        } else {
+            System.out.println("Склад заполнен!");
         }
     }
 
-
+    public static void addBoxToWarehouse(int capacity) {
+        arrayAllBox = new Box[capacity];
+        if (counterWarehouse < capacityWarehouse) {
+            for (int i = 0; i < capacity; i++) {
+                counterWarehouse++;
+                arrayAllBox[i] = new Box(random(), random(), random());
+                System.out.println(arrayAllBox[i]);
+                if (counterWarehouse == capacityWarehouse) {
+                    break;
+                }
+            }
+        } else {
+            System.out.println("Склад заполнен!");
+        }
+    }
 
     public static int valueWarehouseLeft() {
         return capacityWarehouse - counterWarehouse;
@@ -33,4 +58,11 @@ public class Warehouse {
         Random random = new Random();
         return random.nextInt(1, 100);
     }
+
+//    @Override
+//    public String toString(){
+//        StringBuilder resault = new StringBuilder();
+//        resault.append("На складе коробка с длиной = ");
+//        return resault.toString();
+//    }
 }
